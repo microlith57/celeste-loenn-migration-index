1e9d45a8a3664b992025d0328d1c36aec69d74b9
microlith57 <microlith57@gmail.com>
2022-11-07 17:03:26 +1300

    implement app

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,71 @@
 <script>
+  import Helper from "./Helper.svelte";
+
+  let promise = getUnmigratedHelpers();
+
+  function hasUnconvertedPlugin(mod) {
+    return (
+      Object.values(mod.entityList).some(
+        (plugins) => plugins.includes("ahorn") && !plugins.includes("loenn")
+      ) ||
+      Object.values(mod.triggerList).some(
+        (plugins) => plugins.includes("ahorn") && !plugins.includes("loenn")
+      ) ||
+      Object.values(mod.effectList).some(
+        (plugins) => plugins.includes("ahorn") && !plugins.includes("loenn")
+      )
+    );
+  }
+
+  async function getUnmigratedHelpers() {
+    const res = await fetch(
+      `https://max480-random-stuff.appspot.com/celeste/custom-entity-catalog.json`
+    );
+
+    if (res.ok) {
+      let catalog = await res.json();
+      let mods = catalog.modInfo;
+      let helpers = mods.filter(
+        (mod) => mod.categoryName == "Helper" && hasUnconvertedPlugin(mod)
+      );
+      return helpers;
+    } else {
+      throw new Error(await res.text());
+    }
+  }
 </script>
 
 <main>
   <h1>Celeste Lönn Migration Index</h1>
 
   <p>
-    This is a list of all Celeste helpers that still have unmigrated plugins (that is, they have Ahorn
-    plugins without corresponding Lönn plugins), sorted by the number of mods that depend on them.
+    This is a list of all Celeste helpers that still have unmigrated plugins
+    (that is, they have Ahorn plugins without corresponding Lönn plugins).
   </p>
   <p>
-    This is generated automatically (each time you load the page) from <a
+    This list is generated automatically (each time you load the page) from <a
       href="https://max480-random-stuff.appspot.com/celeste/custom-entity-catalog"
       >max480's Celeste Custom Entity and Trigger List</a
-    >, so it updates when that list updates.
+    >, so it updates when that updates.
   </p>
+
+  {#await promise}
+    <p class="loading">Loading...</p>
+  {:then helpers}
+    {#each helpers as mod (mod.itemid)}
+      <Helper {mod} />
+    {/each}
+  {:catch error}
+    <h3>
+      An error occurred when trying to load the Celeste Custom Entity and
+      Trigger List:
+    </h3>
+    <pre><code style="color: red">{error.message}</code></pre>
+  {/await}
 </main>
 
 <style>
+  .loading {
+    color: var(--text-muted);
+  }
 </style>
---
+++ src/Helper.svelte
@@ -0,0 +1,96 @@
+<script>
+  export let mod;
+
+  function unconvertedEntities(mod) {
+    let entities = [];
+    for (const key in mod.entityList) {
+      if (
+        Object.hasOwnProperty.call(mod.entityList, key) &&
+        mod.entityList[key].includes("ahorn") &&
+        !mod.entityList[key].includes("loenn")
+      ) {
+        entities.push(key);
+      }
+    }
+    entities.sort();
+    return entities;
+  }
+
+  function unconvertedTriggers(mod) {
+    let triggers = [];
+    for (const key in mod.triggerList) {
+      if (
+        Object.hasOwnProperty.call(mod.triggerList, key) &&
+        mod.triggerList[key].includes("ahorn") &&
+        !mod.triggerList[key].includes("loenn")
+      ) {
+        triggers.push(key);
+      }
+    }
+    triggers.sort();
+    return triggers;
+  }
+
+  function unconvertedEffects(mod) {
+    let effects = [];
+    for (const key in mod.effectList) {
+      if (
+        Object.hasOwnProperty.call(mod.effectList, key) &&
+        mod.effectList[key].includes("ahorn") &&
+        !mod.effectList[key].includes("loenn")
+      ) {
+        effects.push(key);
+      }
+    }
+    effects.sort();
+    return effects;
+  }
+
+  function numUnconvertedPlugins(mod) {
+    return (
+      unconvertedEntities(mod).length +
+      unconvertedTriggers(mod).length +
+      unconvertedEffects(mod).length
+    );
+  }
+</script>
+
+<article>
+  <h3>
+    {mod.modName} <a href="https://gamebanana.com/mods/{mod.itemid}">🍌</a> ({mod.dependentCount} dependents)
+  </h3>
+  <details>
+    {#if numUnconvertedPlugins(mod) == 1}
+      <summary>1 plugin left</summary>
+    {:else}
+      <summary>{numUnconvertedPlugins(mod)} plugins left</summary>
+    {/if}
+
+    {#if unconvertedEntities(mod).length > 0}
+      <h4>Entities</h4>
+      <ul>
+        {#each unconvertedEntities(mod) as entity}
+          <li>{entity}</li>
+        {/each}
+      </ul>
+    {/if}
+
+    {#if unconvertedTriggers(mod).length > 0}
+      <h4>Triggers</h4>
+      <ul>
+        {#each unconvertedTriggers(mod) as trigger}
+          <li>{trigger}</li>
+        {/each}
+      </ul>
+    {/if}
+
+    {#if unconvertedEffects(mod).length > 0}
+      <h4>Effects</h4>
+      <ul>
+        {#each unconvertedEffects(mod) as effect}
+          <li>{effect}</li>
+        {/each}
+      </ul>
+    {/if}
+  </details>
+</article>
